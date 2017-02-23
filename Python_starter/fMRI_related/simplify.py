2bcedc9d185d295f6a0f67c7ff435abaad9139ad
Narglc <qingcheliuzhi@qq.com>
2017-02-23 21:56:32 +0800

    The completed version with function which can call by python or ipython directly.

--- Python_starter/fMRI_related/simplify.py
+++ Python_starter/fMRI_related/simplify.py
@@ -2,16 +2,15 @@
 #coding:utf-8
 
 #ver3.0
+#Author:Narglc
+#Data:2017.2.23
 #try to change the main code to a function
-#output format: csv
-#使用逗号或tab分割有效信息
+#自主命名输出文件名，但格式为.csv格式
+#即,使用逗号或tab分割有效信息
 
-
-def simplify(filename):
+def simplify(filename,resultfile):
 	con = open(filename,'r')
-	#con = open('fMRI.result','r')
-	out = open('accurate.csv','a')
-	out.write('\n\nResult of ')
+	out = open(resultfile,'a')
 	AAL='narglc'
 	while True:
 		line=con.readline()
@@ -19,32 +18,35 @@ def simplify(filename):
 		if line:
 			if 'byScans' in line:
 				SubNo=line[-22:-16]
-				out.write(SubNo+'\n')           #被试信息
+				out.write('\n\nResult of '+SubNo+'\n')           #被试信息
 			if 'Number of clusters' in line:
-				 listline=line.split(': ')
-				 numOfClusters=listline[1]      #总共多少个Cluster
+				 splitLine=line.split(': ')
+				 numOfClusters=splitLine[1]      #总共多少个Cluster
 				 out.write('Clusters:'+numOfClusters+'\n')
 				 out.write('Voxels,coordinate,anatomy,AAL\n')
 			elif 'Number of voxels' in line:
-				listline=line.split(': ')
-				numOfVoxels=listline[1]         #对应的cluster总共有多少voxels
+				splitLine=line.split(': ')
+				numOfVoxels=splitLine[1]         #对应的cluster总共有多少voxels
 				out.write(numOfVoxels+',')
 			elif 'region' in line:
-				ll=line.split(' // ')
-				ll_tmp=ll[6].split(' ')
-				ll[6]=ll_tmp[0]
-				keep_line=','.join([ll[3],ll[6]])  #峰值点对应脑区
-				AAL=ll[6]
-				out.write(str(keep_line))
+				splitLine=line.split(' // ')
+				splitLine_tmp=splitLine[6].split(' ')
+				AAL=splitLine_tmp[0]
+				keep_line=','.join([splitLine[3],AAL])  #峰值点对应脑区
+				out.write(keep_line)
 				if AAL=='undefined':
 					out.write('\n')
 			elif 'coordinate:' in line:
-				listline = line.split(': ')
-				coordinate = listline[1]        #峰值点对应坐标
+				splitLine = line.split(': ')
+				coordinate = splitLine[1]        #峰值点对应坐标
 				out.write(coordinate+',')
 			elif AAL in line and not('Peak' in line) and AAL!='undefined':
-				listline = line.split('\t')
-				voxels=int(listline[0])
+				if '\t' in line:
+					splitLine = line.split('\t')
+					voxels=int(splitLine[0])
+				else:
+					splitLine = line.split(' ')
+					voxels = splitLine[-3]
 				out.write('('+str(voxels)+'),\n')
 				AAL='narglc'
 		else:
@@ -52,10 +54,15 @@ def simplify(filename):
 	con.close()
 	out.close()
 
-subs=range(10,29)
+subs=range(10,13)								##针对数据进行修改
+resultFile='TestData/out.result'					##输出文件名
 for sub in subs:
-	if sub==22:
-		continue
-	filename='sub0'+str(sub)+'.txt'
-	simplify('TestData/'+filename)
-	print filename+' done.'
+#	if sub==22:									##确定是否剔除数据
+#		continue
+	filename='TestData/sub0'+str(sub)+'.data'
+	simplify(filename,resultFile)
+	print filename[-10:]+' done.'
+
+#filename='TestData/all_fMRI.data'
+#simplify(filename,resultFile)
+print "Done."
