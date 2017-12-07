7783c53b00e714e09fbbd730713c823927b30712
Narglc <qingcheliuzhi@qq.com>
2017-12-07 20:57:06 +0800

    Chapter 01 了解C++的基本运行

--- cpp/first.cpp
+++ cpp/first.cpp
@@ -1,3 +1,17 @@
+#include<iostream>
+
 int main(){
+	//输出文本
+	std::cout<<"Testing\n";
+	//同一语句可延续多行
+	std::cout<<"Testing2\n"
+	"narglc"
+	" is loving "
+	"good";
+	//等待回车继续执行
+	std::cin.get();	
+	//另一种换行方式
+	std::cout<<"testing narglc!"<<std::endl;
 	return 0;
+
 }
