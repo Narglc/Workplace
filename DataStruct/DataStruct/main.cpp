9b4146f110af7a79172430ae1b14a640ccc4994f
Narglc Yu <qingcheliuzhi@qq.com>
2019-05-27 23:22:31 +0800

    begin to review DataStruct.

---
+++ DataStruct/DataStruct/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  DataStruct
+//
+//  Created by Narglc on 2019/5/27.
+//  Copyright © 2019 Narglc. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
