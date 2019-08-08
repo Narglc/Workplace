afc8ee36a3dbda353aad4ffbea1a9a40af458536
Narglc Yu <qingcheliuzhi@qq.com>
2019-08-08 22:36:20 +0800

    Crossin 7天活动 第一天

---
+++ Python_starter/Single7days/firstDay.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+
+# 一刷
+print("Hello World!")
+
+# 二刷
+x=input("pls input x:")
+y=input("pls input y:")
+
+x = int(x)
+y = int(y)
+
+if(x == 0 or y == 0):
+	print("("+str(x)+","+str(y)+")在坐标轴上")
+elif(x > 0 and y > 0):
+	print("("+str(x)+","+str(y)+")在第一象限内")
+elif(x < 0 and y > 0):
+	print("("+str(x)+","+str(y)+")在第二象限内")
+elif(x < 0 and y < 0):
+	print("("+str(x)+","+str(y)+")在第三象限内")
+elif(x > 0 and y < 0):
+	print("("+str(x)+","+str(y)+")在第四象限内")
+
+print("end..")
