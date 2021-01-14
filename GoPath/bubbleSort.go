0fd5f390e0aa26488b358d3191e312b673426f31
narglc <qingcheliuzhi@qq.com>
2021-01-15 00:46:28 +0800

    bubble sort implement

--- GoPath/bubbleSort.go
+++ GoPath/bubbleSort.go
@@ -5,7 +5,20 @@ import (
 )
 
 func main() {
-	a := [...]int{5, 2, 6, 3, 9}
+	a := [...]int{10,3,66,23,33,9,5, 2, 6, 3, 9}
 	fmt.Println("orig: ", a)
 
+	Len:=len(a)
+	fmt.Println("len(a)=",Len)
+	for i:=0;i<Len;i++ {
+		for j:=0;j<Len-1-i;j++ {
+		    if a[j] > a[j+1] {
+				tmp:= a[j]
+				a[j]=a[j+1]
+				a[j+1]=tmp
+			}
+		}
+		fmt.Println("->",a)
+	}
+	fmt.Println("after: ",a)
 }
